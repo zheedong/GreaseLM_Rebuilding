411cd5263f1f9b465be9ec54c3e5be37980dceb8
zheedong <zheedong@gmail.com>
2023-02-07 01:59:34 +0900

    Rebuilding

--- modeling/rebuild_modeling_gnn.py
+++ modeling/rebuild_modeling_gnn.py
@@ -9,6 +9,7 @@ from torch_scatter import scatter
 
 from einops import rearrange
 
+
 def make_one_hot(labels, C):
     labels = rearrange(labels, 'n -> n 1')
     one_hot = torch.zeros(labels.size(0), C).to(labels.device)
@@ -19,21 +20,78 @@ def make_one_hot(labels, C):
     target = one_hot.scatter_(1, labels, 1)
     return target
 
+
 class GATConvE(MessagePassing):
-    def __init__(self, emb_dim, n_ntype, n_etype, edge_encoder, head_count=4, aggr="add"):
+    def __init__(
+            self,
+            emb_dim,
+            n_ntype,
+            n_etype,
+            edge_encoder,
+            head_count=4,
+            aggr="add"):
         super(GATConvE, self).__init__(aggr=aggr)
 
         # emb_dim must be even
         assert emb_dim % 2 == 0
 
         self.emb_dim = emb_dim
+        # number of node types. {Z, Q, A, O}
         self.n_ntype = n_ntype
+        # number of edge relation. 38
         self.n_etype = n_etype
         self.edge_encoder = edge_encoder
 
         # For Attention
         self.head_count = head_count
         assert emb_dim % head_count == 0
+        self.dim_per_head = emb_dim // head_count
+        self.linear_key = nn.Linear(
+            3 * emb_dim, head_count * self.dim_per_head)
+        self.linear_msg = nn.Linear(
+            3 * emb_dim, head_count * self.dim_per_head)
+        self.linear_query = nn.Linear(
+            2 * emb_dim, head_count * self.dim_per_head)
 
+        self._alpha = None
+
+        # For Final MLP
+        self.mlp = nn.Sequential(torch.nn.Linear(emb_dim, emb_dim),
+                                 torch.nn.BatchNorm1d(emb_dim),
+                                 torch.nn.ReLU(),
+                                 torch.nn.Linear(emb_dim, emb_dim))
+
+    def forward(
+            self,
+            x,
+            edge_index,
+            edge_type,
+            node_type,
+            node_feature_extra,
+            return_attention_weights=False):
+        # Prepare edge features
+
+        # Why + 1? Maybe because of the interaction token?
+        edge_vec = make_one_hot(edge_type, self.n_etype + 1)
+        self_edge_vec = torch.zeros(
+            x.size(0),
+            self.n_etype +
+            1).to(
+            edge_vec.device)
+        # TODO : Understand this. Why??
+        self_edge_vec[:, self.n_etype] = 1
+
+        head_type = node_type[edge_index[0]]
+        tail_type = node_type[edge_index[1]]
+        head_vec = make_one_hot(head_type, self.n_ntype)
+        tail_vec = make_one_hot(tail_type, self.n_ntype)
+        headtail_vec = torch.cat([head_vec, tail_vec], dim=1)
+        self_head_vec = make_one_hot(node_type, self.n_ntype)
+        self_headtail_vec = torch.cat([self_head_vec, self_head_vec], dim=1)
+
+        edge_vec = torch.cat([edge_vec, self_edge_vec], dim=0)
+        headtail_vec = torch.cat([headtail_vec, self_headtail_vec], dim=0)
+        edge_embeddings = self.edge_encoder(
+            torch.cat([edge_vec, headtail_vec], dim=1))
 
     raise NotImplementedError
--- modeling/rebuild_modeling_greaselm.py
+++ modeling/rebuild_modeling_greaselm.py
@@ -25,30 +25,149 @@ from modeling import modeling_gnn
 from utils import layers
 from utils import utils
 
+
 class GreaseLM(nn.Module):
-    def __init__(self, args={}, model_name="roberta-base", k=5, n_ntype=4, n_etype=38,
-                 n_concepts=799273, concept_dim=200, concept_int_dim=1024, n_attention_head=2,
-                 fc_dim=200, n_fc_layer=0, p_emb=0.2, p_gnn=0.2, p_cf=0.2,
-                 pretrained_concept_emb=None, freeze_ent_emb=True,
-                 init_range=0.02, ie_dim=200, info_exchange=True, ie_layer_num=1, sep_ie_layers=False, layer_id=-1):
+    def __init__(
+            self,
+            args={},
+            model_name="roberta-base",
+            k=5,
+            n_ntype=4,
+            n_etype=38,
+            n_concepts=799273,
+            concept_dim=200,
+            concept_int_dim=1024,
+            n_attention_head=2,
+            fc_dim=200,
+            n_fc_layer=0,
+            p_emb=0.2,
+            p_gnn=0.2,
+            p_cf=0.2,
+            pretrained_concept_emb=None,
+            freeze_ent_emb=True,
+            init_range=0.02,
+            ie_dim=200,
+            info_exchange=True,
+            ie_layer_num=1,
+            sep_ie_layers=False,
+            layer_id=-1):
         super().__init__()
-        self.lmgnn = LMGNN(args, model_name, k, n_ntype, n_etype, n_concepts, concept_dim, concept_int_dim, n_attention_head,
-                                fc_dim, n_fc_layer, p_emb, p_gnn, p_cf, pretrained_concept_emb, freeze_ent_emb,
-                                init_range, ie_dim, info_exchange, ie_layer_num, sep_ie_layers, layer_id)
-        raise NotImplementedError
+        self.lmgnn = LMGNN(
+            args,
+            model_name,
+            k,
+            n_ntype,
+            n_etype,
+            n_concepts,
+            concept_dim,
+            concept_int_dim,
+            n_attention_head,
+            fc_dim,
+            n_fc_layer,
+            p_emb,
+            p_gnn,
+            p_cf,
+            pretrained_concept_emb,
+            freeze_ent_emb,
+            init_range,
+            ie_dim,
+            info_exchange,
+            ie_layer_num,
+            sep_ie_layers,
+            layer_id)
+
+        def batch_graph(self, edge_index_init, edge_type_init, n_nodes):
+            raise NotImplementedError
+
+        def forward(self, *inputs, cache_output=False, detail=False):
+            raise NotImplementedError
+
+        def get_fake_inputs(self, device="cuda:0"):
+            raise NotImplementedError
+
+        def get_fake_inputs(self, device="cuda:0"):
+            raise NotImplementedError
+
+        def check_outputs(self, logits, attn):
+            raise NotImplementedError
+
+
+def test_GreaseLM():
+    raise NotImplementedError
+
 
 class LMGNN(nn.Module):
-    def __init__(self, args={}, model_name="roberta-base", k=5, n_ntype=4, n_etype=38,
-                 n_concepts=799273, concept_dim=200, concept_int_dim=1024, n_attention_head=2,
-                 fc_dim=200, n_fc_layer=0, p_emb=0.2, p_gnn=0.2, p_cf=0.2,
-                 pretrained_concept_emb=None, freeze_ent_emb=True,
-                 init_range=0.02, ie_dim=200, info_exchange=True, ie_layer_num=1, sep_ie_layers=False, layer_id=-1):
+    def __init__(
+            self,
+            args={},
+            model_name="roberta-base",
+            k=5,
+            n_ntype=4,
+            n_etype=38,
+            n_concepts=799273,
+            concept_dim=200,
+            concept_int_dim=1024,
+            n_attention_head=2,
+            fc_dim=200,
+            n_fc_layer=0,
+            p_emb=0.2,
+            p_gnn=0.2,
+            p_cf=0.2,
+            pretrained_concept_emb=None,
+            freeze_ent_emb=True,
+            init_range=0.02,
+            ie_dim=200,
+            info_exchange=True,
+            ie_layer_num=1,
+            sep_ie_layers=False,
+            layer_id=-1):
         super().__init__()
+        raise NotImplementedError
 
+    def _init_weights(self, module):
+        raise NotImplementedError
+
+    def forward(
+            self,
+            inputs,
+            concept_ids,
+            node_type_ids,
+            node_scores,
+            adj_lengths,
+            special_nodes_mask,
+            adj,
+            emb_data=None,
+            cache_output=False):
+        raise NotImplementedError
+
+    def get_fake_inputs(self, device="cuda:0"):
+        raise NotImplementedError
+
+    def check_outputs(self, logits, pool_attn):
+        raise NotImplementedError
+
+
+def test_LMGNN():
     raise NotImplementedError
+
 
 class TextKGMessagePassing(ModelClass):
-    raise NotImplementedError
+    def __init__(
+            self,
+            config,
+            args={},
+            k=5,
+            n_ntype=4,
+            n_etype=38,
+            dropout=0.2,
+            concept_dim=200,
+            ie_dim=200,
+            p_fc=0.2,
+            info_exchange=True,
+            ie_layer_num=1,
+            sep_ie_layers=False):
+        raise NotImplementedError
+
 
 class RoBERTaGAT(modeling_bert.BertEncoder):
     raise NotImplementedError
