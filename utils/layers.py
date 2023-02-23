8809c12af7c55c520a127c8a28291d5966b2ff6b
zheedong <zheedong@gmail.com>
2023-02-24 02:04:08 +0900

    Dimension fixed

--- utils/layers.py
+++ utils/layers.py
@@ -133,7 +133,7 @@ class ExchangeResidualConnect(nn.Module):
     def forward(self, inp):
         wandb.log({"alpha":self.linear_combination.weight.data[0]})
         wandb.log({"beta":self.linear_combination.weight.data[1]})
-        return self.linear_combination(torch.stack([self.exchange(inp), inp]))
+        return self.linear_combination(torch.stack([self.exchange(inp), inp], dim=1))
 
 
 ######################################################
